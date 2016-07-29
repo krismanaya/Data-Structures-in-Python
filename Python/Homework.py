15241f5074cac70dbc484c87ba5ed02b530cb79e
krismanaya <krismanaya@gmail.com>
2016-07-29 14:19:36 -0700

    Make a choice

--- Python/Homework.py
+++ Python/Homework.py
@@ -1,4 +1,12 @@
+import random
+
 ## Section 1 ##
 ## This function returns powers of two up to i. 
 def power_of_two(i): 
     return [2**k for k in range(0,i)]
+
+#This function returns a random choice from the given range of sequence. 
+def choice(data): 
+    if not isinstance(data,list): 
+        raise TypeError('data must be list type')
+    return data[random.randrange(len(data))]
--- Python/tests/homeworktests.py
+++ Python/tests/homeworktests.py
@@ -13,4 +13,6 @@ class HomeworkSequenceFunctions(unittest.TestCase):
         expected = [1,2,4,8,16,32,64,128,256]
         actual = Homework.power_of_two(9)
         self.assertEqual(expected,actual)
-        
+
+    def test_choice(self):
+        self.assertTrue(False)
