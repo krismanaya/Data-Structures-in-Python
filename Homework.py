7ed949ca509fb2dcf4a547429133b1a064d28866
krismanaya <krismanaya@gmail.com>
2016-07-29 02:23:36 -0700

    Exponentiate

--- Homework.py
+++ Homework.py
@@ -1,5 +1,4 @@
 ## Section 1 ##
-## This function returns powers of two up to 8. 
-def power_of_two(): 
-    return [2**k for k in range(0,9)]
-    
+## This function returns powers of two up to i. 
+def power_of_two(i): 
+    return [2**k for k in range(0,i)]
