44a0e0f4d66341ffb3348d6e1f6baf8fd87c6ec7
krismanaya <krismanaya@gmail.com>
2016-07-30 13:53:03 -0700

    Change TypeError

--- Python/Homework.py
+++ Python/Homework.py
@@ -5,7 +5,7 @@ import random
 ## This function returns powers of two up to i. 
 def power_of_two(i):
     if not isinstance(i,int): 
-        raise TypeError('data must be int type') 
+        raise TypeError('i must be int type') 
     return [2**k for k in range(0,i)]
 
 #This function returns a random choice from the given range of sequence. 
